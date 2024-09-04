ad71f370c548f0154fa0147d74e82ea545d6077b
Mohammad <mohammad8mohammadi4@gmail.com>
2024-09-04 22:15:12 +0330

    working

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function setFood() {
    while (true) {
       let randomRow = Math.floor(Math.random() * 10);
       let randomCol = Math.floor(Math.random() * 10);
-      console.log(randomRow + " " + randomCol);
+      //console.log(randomRow + " " + randomCol);
       if (houses[randomRow][randomCol].classList == "") {
          houses[randomRow][randomCol].classList = "food";
          break;
@@ -24,72 +24,81 @@ function setFood() {
    }
 }
 setFood();
-// function going(){
-//    if(continuing){
-//       if(goingType=='Right'){
+let head = houses[6][4];
+let tail = houses[6][2];
+let Row = 6,
+   Col = 4;
+console.log(head)
+console.log(tail)
+function checkFinished(){
+   if(Row==10||Col==10||Row==-1||Col==-1){
+      continuing = false
+   }
+}
+function go() {
+   body[0].classList = "circle";
+   if (goingType == "Right") {Col++
+      houses[Row][Col].classList = "head";
+   } else if (goingType == "Left") {Col--
+      houses[Row][Col].classList = "head";
+   } else if (goingType == "Up") {Row--
+      houses[Row][Col].classList = "head";
+   } else if (goingType == "Down") {Row++
+      houses[Row][Col].classList = "head";
+   }
+   body.unshift(houses[Row][Col]);
+   head = body[0];
+   tail = body.pop();
+   tail.classList = "";
+   tail = body[body.length - 1];
+   checkFinished()
+}
 
-//       }else if(goingType=='Left'){
-
-//       }else if(goingType=='Up'){
-
-//       }else if(goingType=='Down'){
-
+//go()
+// function findHead() {
+//    let Row, Col;
+//    for (let i = 0; i < 10; i++) {
+//       for (let j = 0; j < 10; j++) {
+//          if (houses[i][j] == body[0]) {
+//             Row = i;
+//             Col = j;
+//             return [Row, Col];
+//          }
 //       }
 //    }
 // }
-// let rcnt = 0
-// while (rcnt<3) {
-//    setTimeout(() => {
-//       let head = body[0];
-//       head.classList = "circle";
-
-//       let tail = body.pop();
-//       tail.classList = "";
-//    }, seconds[secondsCounter]*rcnt);rcnt++
-// }
-let counter = 1;
-function go() {
-   //setTimeout(() => {
-   body[0].classList = "circle";
-   houses[6][4 + counter].classList = "head";
-   body.unshift(houses[6][4 + counter]);
-   counter++;
-   let tail = body.pop();
-   tail.classList = "";
-   //}, seconds[secondsCounter]);
-}
-
-for (let index = 1; index <= 3; index++) {
-   setTimeout(() => {
-      go();
-   }, seconds[secondsCounter] * index);
-   // if(index==2)secondsCounter++
-}
-
 function goUp() {
    if (goingType == "Right" || goingType == "Left") {
-      let Row, Col;
-      for (let i = 0; i < 10; i++) {
-         for (let j = 0; j < 10; j++) {
-            if (houses[i][j] == body[0]) {
-               Row = i;
-               Col = j;
-            }
-         }
-      }
+      //let [Row, Col] = findHead();
       body[0].classList = "circle";
-      houses[Row-1][Col].classList = "head";
-      houses[Row-1][Col].style.backgroundImage='url(Pictures/SnakeHeadUp.png)'
-      body.unshift(houses[Row-1][Col]);
+      houses[Row - 1][Col].classList = "head";
+      houses[Row - 1][Col].style.backgroundImage =
+         "url(Pictures/SnakeHeadUp.png)";
+      body.unshift(houses[Row - 1][Col]);
       Row--;
-      let tail = body.pop();
+      tail = body.pop();
       tail.classList = "";
+      goingType = "Up";
+   }
+}
+function goRight() {
+   if (goingType == "Up" || goingType == "Down") {
+      let [Row, Col] = findHead();
+      body[0].classList = "circle";
+      houses[Row][Col + 1].classList = "head";
+      houses[Row][Col + 1].style.backgroundImage =
+         "url(Pictures/SnakeHeadRight.png)";
+      body.unshift(houses[Row - 1][Col]);
+      Col++;
+      tail = body.pop();
+      tail.classList = "";
+      goingType = "Right";
    }
 }
 document.addEventListener("keydown", (key) => {
    if (continuing) {
       if (key.code == "ArrowUp") {
-         goUp();
+         continuing = false
       } else if (key.code == "ArrowDown") {
          goDown();
       } else if (key.code == "ArrowLeft") {
@@ -97,5 +106,16 @@ document.addEventListener("keydown", (key) => {
       } else if (key.code == "ArrowRight") {
          goRight();
       }
+      go();
    }
 });
+cnt = 1;
+while (continuing) {
+   setTimeout(() => {
+      go();
+   }, seconds[secondsCounter] * cnt);
+   cnt++;
+   if(cnt==10000){
+      break
+   }
+}
